3a986129c8284d1cbc82f38dfc158c82fe98b40a
Carmen <38572315+CarmenGV@users.noreply.github.com>
2019-09-13 22:06:43 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,82 @@
-//PORTFOLIO CARDS
-const portImage = document.getElementsByClassName('port-img');
-const portDesc = document.getElementsByClassName('port-desc');
-const portButton = getElementsByClassName('port-btn');
+const portfolio = [
+  {
+    title: "Author Website",
+    image: "img/authorWebsite.jpg",
+    desc: [
+      "Responsive Design",
+      "Sass",
+      "JQuery",
+      "PHP"
+    ],
+    links: {
+      live: "http://isabelapowers.com/"
+    }
+  },
+  {
+    title: "Webstyle Guide",
+    image: "img/styleGuide.jpg",
+    desc: [
+      "Responsive Design",
+      "Sass"
+    ],
+    links: {
+      live: "projects/webstyle/index.html",
+      github: "https://github.com/CarmenGV/Techdegree-BuildWebstyleGuide"
+    }
+  },
+  {
+    title: "Online Form",
+    image: "img/onlineForm.jpg",
+    desc: [
+      "Responsive Design",
+      "CSS"
+    ],
+    links: {
+      live: "projects/form/index.html",
+      github: "https://github.com/CarmenGV/Techdegree-OnlineRegistration"
+    }
+  },
+  {
+    title: "Game App",
+    image: "img/gameApp.jpg",
+    desc: [
+      "JavaScript",
+      "DOM Manipulation"
+    ],
+    links: {
+      live: "projects/gameApp/index.html",
+      github: "https://github.com/CarmenGV/Techdegree-GameShowApp"
+    }
+  },
+  {
+    title: "Web App Dashboard",
+    image: "img/webApp.jpg",
+    desc: [
+      "Responsive Design",
+      "JQuery",
+      "JavaScript",
+      "SVG Graphics"
+    ],
+    links: {
+      live: "projects/webApp/index.html",
+      github: "https://github.com/CarmenGV/Techdegree-WebAppDashboard"
+    }
+  },
+  {
+    title: "Employee Directory",
+    image: "img/employeeDirectory.jpg",
+    desc: [
+      "Responsive Design",
+      "API Data Fetching",
+      "JavaScript"
+    ],
+    links: {
+      live: "projects/directory/index.html",
+      github: "https://github.com/CarmenGV/Techdegree-EmployeeDirectory"
+    }
+  }
+];
 
-//Hover Event Function (portfolio)
+function portfolioLI(arr){
+  
+}
