2305ec204d56395b9022cd9961a8bdf1dc228e31
Carmen <38572315+CarmenGV@users.noreply.github.com>
2019-09-17 16:30:45 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,82 +1,24 @@
-const portfolio = [
-  {
-    title: "Author Website",
-    image: "img/authorWebsite.jpg",
-    desc: [
-      "Responsive Design",
-      "Sass",
-      "JQuery",
-      "PHP"
-    ],
-    links: {
-      live: "http://isabelapowers.com/"
-    }
-  },
-  {
-    title: "Webstyle Guide",
-    image: "img/styleGuide.jpg",
-    desc: [
-      "Responsive Design",
-      "Sass"
-    ],
-    links: {
-      live: "projects/webstyle/index.html",
-      github: "https://github.com/CarmenGV/Techdegree-BuildWebstyleGuide"
-    }
-  },
-  {
-    title: "Online Form",
-    image: "img/onlineForm.jpg",
-    desc: [
-      "Responsive Design",
-      "CSS"
-    ],
-    links: {
-      live: "projects/form/index.html",
-      github: "https://github.com/CarmenGV/Techdegree-OnlineRegistration"
-    }
-  },
-  {
-    title: "Game App",
-    image: "img/gameApp.jpg",
-    desc: [
-      "JavaScript",
-      "DOM Manipulation"
-    ],
-    links: {
-      live: "projects/gameApp/index.html",
-      github: "https://github.com/CarmenGV/Techdegree-GameShowApp"
-    }
-  },
-  {
-    title: "Web App Dashboard",
-    image: "img/webApp.jpg",
-    desc: [
-      "Responsive Design",
-      "JQuery",
-      "JavaScript",
-      "SVG Graphics"
-    ],
-    links: {
-      live: "projects/webApp/index.html",
-      github: "https://github.com/CarmenGV/Techdegree-WebAppDashboard"
-    }
-  },
-  {
-    title: "Employee Directory",
-    image: "img/employeeDirectory.jpg",
-    desc: [
-      "Responsive Design",
-      "API Data Fetching",
-      "JavaScript"
-    ],
-    links: {
-      live: "projects/directory/index.html",
-      github: "https://github.com/CarmenGV/Techdegree-EmployeeDirectory"
-    }
-  }
+const simple = [
+  document.querySelector('#about h3'),
+  document.querySelector('#skills h3'),
+  document.querySelector('#portfolio h3'),
+  document.querySelector('#contact h3'),
+  document.querySelector('.about-desc')
 ];
+const slideUp = [
+  document.querySelector('.about-cards'),
+  document.querySelector('.skills-chart'),
+  document.querySelector('#portGrid'),
+  document.querySelector('#contact-info')
+]
 
-function portfolioLI(arr){
-  
-}
+ScrollReveal().reveal(simple,{
+  delay: 300,
+  easing: 'ease-in-out',
+});
+ScrollReveal().reveal(slideUp, {
+  distance: '50px',
+  origin: 'bottom',
+  delay: 500,
+  duration: 900,
+});
